3202782e95a5a19941a9fe480a1f47e0c20bb4a9
Ryan Nóbrega <ryanbrandao18@gmail.com>
2022-03-04 23:24:29 -0300

    adicionando array de dependentes

--- app/js/model/cliente-especial.js
+++ app/js/model/cliente-especial.js
@@ -3,5 +3,6 @@
 class ClienteEspecial extends Cliente {
     constructor(nome, cpf, conta) {
         super(nome, cpf, conta);
+        this._dependentes = new Array();
     }
 }
--- app/ts/model/cliente-especial.ts
+++ app/ts/model/cliente-especial.ts
@@ -4,5 +4,6 @@ class ClienteEspecial extends  Cliente{
     private _dependentes: Array<Cliente>
     constructor(nome:string, cpf: string, conta: Conta) {
         super(nome,cpf,conta);
+        this._dependentes = new Array<Cliente>()
     }
 }
